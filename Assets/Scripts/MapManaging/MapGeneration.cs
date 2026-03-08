f66b570a0a1bcb5414467c8b80f9ba428a481e98
Adieru <sushilaimadol@gmail.com>
2026-03-08 17:55:34 +0545

    Infinite map generation

--- Assets/Scripts/MapManaging/MapGeneration.cs
+++ Assets/Scripts/MapManaging/MapGeneration.cs
@@ -8,5 +8,17 @@ public class MapGeneration : MonoBehaviour
     [SerializeField] Transform BasketsHolder;
 
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {           
+        if (collision.CompareTag("Basket") && collision.transform.GetSiblingIndex() == 2)
+        {
+            BasketsHolder.GetChild(0).transform.position = BasketsHolder.GetChild(BasketsHolder.childCount - 1).transform.position 
+                                                            + new Vector3(5, 0, 0);
+
+            BasketsHolder.GetChild(0).SetSiblingIndex(BasketsHolder.childCount - 1);
+        }
+
+    }
+
 
 }
