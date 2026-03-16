dd3171d651083102216db5955308061532d0d25b
Adieru <sushilaimadol@gmail.com>
2026-03-16 11:44:15 +0545

    Player game over fix, now onto score

--- Assets/Scripts/PlayerFocused/GameOverManagement.cs
+++ Assets/Scripts/PlayerFocused/GameOverManagement.cs
@@ -7,6 +7,30 @@ public class GameOverManagement : MonoBehaviour
 
     [SerializeField] bool GameOver;
 
+    private Vector3 EnterStart_Pos;
+    private Vector3 EnterEnd_Pos;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Basket"))
+        {
+            EnterStart_Pos = this.transform.position;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Basket"))
+        {
+            EnterEnd_Pos = this.transform.position;
+        }
+
+        if((Mathf.Abs(EnterStart_Pos.y) > Mathf.Abs(EnterEnd_Pos.y)) && GameOver)
+        {
+            GameOverFunc();
+        }
+    }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
