7549b597a41ebadb301fed8f39b0bc778e93c7a8
Adieru <sushilaimadol@gmail.com>
2026-03-16 18:44:01 +0545

    Game Over almost done

--- Assets/Scripts/PlayerFocused/GameOverManagement.cs
+++ Assets/Scripts/PlayerFocused/GameOverManagement.cs
@@ -6,15 +6,36 @@ public class GameOverManagement : MonoBehaviour
 {
 
     [SerializeField] bool GameOver;
+    bool HasCrossedSafeDistance = false;
 
-    private Vector3 EnterStart_Pos;
-    private Vector3 EnterEnd_Pos;
+    [SerializeField] float SafeDistance;
+
+    private Vector3 Enter_Pos;
+    private Vector3 Exit_Pos;
+
+
+    private void Start()
+    {
+        Debug.Log("Check");
+        Enter_Pos = this.transform.position;
+    }
+
+    private void Update()
+    {
+        float distance = Vector2.Distance(this.transform.position, Enter_Pos);
+
+        if(distance > SafeDistance && !HasCrossedSafeDistance && GameOver)
+        {
+            Debug.Log("GameOver");
+            
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Basket"))
         {
-            EnterStart_Pos = this.transform.position;
+            Enter_Pos = this.transform.position;
         }
     }
 
@@ -22,10 +43,10 @@ public class GameOverManagement : MonoBehaviour
     {
         if(collision.CompareTag("Basket"))
         {
-            EnterEnd_Pos = this.transform.position;
+            Exit_Pos = this.transform.position;
         }
 
-        if((Mathf.Abs(EnterStart_Pos.y) > Mathf.Abs(EnterEnd_Pos.y)) && GameOver)
+        if((Mathf.Abs(Enter_Pos.y) > Mathf.Abs(Exit_Pos.y)) && GameOver)
         {
             GameOverFunc();
         }
@@ -43,6 +64,8 @@ public class GameOverManagement : MonoBehaviour
 
     private void GameOverFunc()
     {
+
+
         Time.timeScale = 0;
 
 
